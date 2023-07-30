44fb647369cd34c6ce535bd58d7fb5fd5a98bed3
FaisalM <122456185+Faisal-Algabni@users.noreply.github.com>
2023-07-30 19:58:20 +0300

    ..

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const app = express();
 const port = 3001;
 const middleware = require('./middleware');
 const path = require('path');
+const bodyParser = require("body-parser");
+
 
 
 // run server at http://localhost:3001/
@@ -11,6 +13,8 @@ const server = app.listen(port, () => console.log("Server listening on port "+po
 // setup pug
 app.set("view engine", "pug");
 app.set("views","views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // path to css and js files
 app.use(express.static(path.join(__dirname, "public")));
--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,16 +1,36 @@
 const express = require('express');
 const app = express();
 const router = express.Router();
+const bodyParser = require("body-parser");
 
 app.set("view engine", "pug");
 app.set("views","views");
+
+app.use(bodyParser.urlencoded({ extended: false }));
 
 router.get("/", (req, res,next) => {
     res.status(200).render("register");
 })
 
 router.post("/", (req, res,next) => {
-    res.status(200).render("register");
+
+    var firstName = req.body.firstName.trim();
+    var lastName = req.body.lastName.trim();
+    var username = req.body.username.trim();
+    var email = req.body.email.trim();
+    var password = req.body.password;
+
+    var payload = req.body;
+
+    if(firstName && lastName && username && email && password){
+        
+    } else {
+        // if there is error the message will show off in the form 
+        payload.errorMessage = "Make sure each field has a valid value.";
+        res.status(200).render("register", payload);
+    }
+
+    
 })
 
 module.exports = router;
